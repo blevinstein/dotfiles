81ffef6e4c0c03331ca84cfa38402362c88629f3
Brian Levinstein <blevinstein@gmail.com>
2025-12-26 19:25:44 -0700

    Re-enable copy in setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ git submodule update
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#cp -Rv ./.* $HOME/
-#cp -Rv ./* $HOME/
+cp -Rv ./.* $HOME/
+cp -Rv ./* $HOME/
 
